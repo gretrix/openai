c92c1a1879cc3da216db4d3875f0698ab1a2fec2
gretrix101 <jtremblay@gretrix.com>
2023-03-06 07:45:25 -0500

    Fixing git pull script

--- pull.php
+++ pull.php
@@ -6,6 +6,7 @@
 //echo shell_exec('git config --global --add safe.directory C:/inetpub/wwwroot 2>&1');
 
 
-echo shell_exec('cd C:\inetpub\wwwroot && git config --global --add safe.directory C:/inetpub/wwwroot && git status 2>&1');
+echo shell_exec('cd C:\inetpub\wwwroot && git reset --hard HEAD && git pull && git status 2>&1');
+
 
 ?>
