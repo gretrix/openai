519712f8737b173a78a8d193061248f15fdf987c
gretrix101 <jtremblay@gretrix.com>
2023-03-06 07:30:30 -0500

    More testing

--- pull.php
+++ pull.php
@@ -1,5 +1,11 @@
 <?php 
 
+//echo shell_exec('whoami 2>&1');
+
+
+//echo shell_exec('git config --global --add safe.directory C:/inetpub/wwwroot 2>&1');
+
+
 echo shell_exec('cd C:\inetpub\wwwroot && git config --global --add safe.directory C:/inetpub/wwwroot && git status 2>&1');
 
 ?>
